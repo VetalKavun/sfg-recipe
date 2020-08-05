d8a8515264ed742e32f91325e9567b7f2cecfae8
VetalKavun <vitaliykavun@2014@gmail.com>
2020-08-05 23:18:33 +0300

    initial commit

---
+++ src/test/java/guru/springframwork/sfgrecipe/SfgrecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframwork.sfgrecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgrecipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
